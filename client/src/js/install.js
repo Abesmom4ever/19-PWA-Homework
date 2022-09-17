eb8365a3249052aa02d005e99f77f8d1754565d9
Rejean Chung <rejeanpc@gmail.com>
2022-09-17 12:54:02 -0700

    first

---
+++ client/src/js/install.js
@@ -0,0 +1,26 @@
+const butInstall = document.getElementById("buttonInstall");
+
+// Logic for installing the PWA
+window.addEventListener("beforeinstallprompt", (event) => {
+    window.deferredPrompt = event;
+
+    installBtn.classList.toggle("hidden", false);
+});
+
+butInstall.addEventListener("click", async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    promptEvent.prompt();
+
+    window.deferredPrompt = null;
+
+    installBtn.classList.toggle("hidden", true);
+});
+
+window.addEventListener("appinstalled", (event) => {
+    window.deferredPrompt = null;
+});
