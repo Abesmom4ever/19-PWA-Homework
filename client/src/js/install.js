e1d2c5316144680b5526d31242da8af5acf9c405
Rejean Chung <rejeanpc@gmail.com>
2022-09-17 13:14:28 -0700

    updated src

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,6 +1,6 @@
 const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
+// Installing PWA
 window.addEventListener("beforeinstallprompt", (event) => {
     window.deferredPrompt = event;
 
